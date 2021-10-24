065bbeeb91b0b42e0a7936b447d8daf07dadbb12
kato-toranosuke <toranosuke11041999@gmail.com>
2021-10-24 16:43:36 +0900

    [A] add load_csv.py and ml_pipeline_components.py

---
+++ mylib/load_csv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import pandas as pd
+from typing import List
+
+def CsvToDf(csv_list: List, CSV_PATH: str) -> pd.DataFrame:
+  df = pd.concat([pd.read_csv(CSV_PATH + filename) for filename in csv_list])
+  return df
