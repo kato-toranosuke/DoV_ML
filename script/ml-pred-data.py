ebf76660c79d9a6f5197aff4cc1accb36bb686f7
kato-toranosuke-mbp <toranosuke11041999@gmail.com>
2021-11-29 12:54:03 +0900

    [M] add predict function

--- script/ml-pred-data.py
+++ script/ml-pred-data.py
@@ -15,16 +15,23 @@ from mylib.ml_pipeline_components import MlPipeline
 from mylib.record_model_data import RecExpResultToMarkdown
 
 class MlPred():
-    def __init__(self, train_filename_list: List = None, test_filename_list: List = None, input_pkl_path: str = None, output_pkl_path: str = None, csv_filename: str = None) -> None:
-        self.consts = Exp_Consts(dataset_path='../../experiment_dataset/2021-11-26',
+    def __init__(self, train_filename_list: List = None, test_filename_list: List = None, input_pkl_filename: str = None, output_pkl_filename: str = None, csv_filename: str = None) -> None:
+        self.consts = Exp_Consts(dataset_path='../../experiment_dataset/2021-11-29_mac',
                                  train_csv_path='../out/csv',
                                  test_csv_path='../out/csv/experiment',
-                                 output_path='../out/experiment_result',
-                                 fitting_pkl_path='../out/ml_model_fitting_result')
+                                 input_pkl_path='../out/ml_model_result',
+                                 output_pkl_path='../out/ml_model_fitting_result',
+                                 output_path='../out/experiment_result')
+        # self.consts = Exp_Consts(dataset_path='../../experiment_dataset/2021-11-26',
+        #                          train_csv_path='../out/csv',
+        #                          test_csv_path='../out/csv/experiment',
+        #                          input_pkl_path='../out/ml_model_result',
+        #                          output_pkl_path='../out/ml_model_fitting_result',
+        #                          output_path='../out/experiment_result')
         self.train_filename_list = train_filename_list
         self.test_filename_list = test_filename_list
-        self.input_pkl_path = input_pkl_path
-        self.output_pkl_path = output_pkl_path
+        self.input_pkl_filename = input_pkl_filename
+        self.output_pkl_filename = output_pkl_filename
         self.csv_filename = csv_filename
 
     def wav2csv(self):
@@ -32,7 +39,7 @@ class MlPred():
         createWav2Csv(self.csv_filename, self.consts.DATASET_PATH,
                       self.consts.TEST_CSV_PATH, w=1, N=2**12, overlap=80)
 
-    def fit(self, train_filename_list: List = None, train_csv_path: str = None, train_set_trial: List = None, input_pkl_path: str = None):
+    def fit(self, train_filename_list: List = None, train_csv_path: str = None, train_set_trial: List = None, input_pkl_filename: str = None, output_pkl_filename: str = None):
         # [training data] csv -> DataFrame
         if train_filename_list == None:
             train_filename_list = self.train_filename_list
@@ -55,16 +62,76 @@ class MlPred():
         print('特徴量とラベルの分離完了')
 
         # 学習
-        if input_pkl_path == None:
-            input_pkl_path = self.input_pkl_path
-        model = joblib.load(input_pkl_path)
+        if input_pkl_filename == None:
+            input_pkl_filename = self.input_pkl_filename
+        input_pkl_filepath = self.consts.INPUT_PKL_PATH + '/' + \
+            input_pkl_filename + '/' + input_pkl_filename + '.pkl'
+        model = joblib.load(input_pkl_filepath)
         model.fit(X_train, y_train)
         print('学習完了')
 
         # fittingした結果をpklへ保存する
-        pkl_filepath = self.consts.FITTING_PKL_PATH + '/' + self.output_pkl_path
-        joblib.dump(model, pkl_filepath)
-        print(f'pklへの出力: {pkl_filepath}')
+        if output_pkl_filename == None:
+            output_pkl_filename = self.output_pkl_filename
+        output_pkl_filepath = self.consts.OUTPUT_PKL_PATH + '/' + output_pkl_filename
+        joblib.dump(model, output_pkl_filepath)
+        print(f'pklへの出力: {output_pkl_filepath}')
+
+    def predict(self, test_filename_list: List = None, test_csv_path: str = None, test_set_trial: List = None, input_pkl_filename: str = None):
+        # [test data] csv -> DataFrame
+        if test_filename_list == None:
+            test_filename_list = self.test_filename_list
+        if test_csv_path == None:
+            test_csv_path = self.consts.TEST_CSV_PATH
+        df_test = CsvToDf(test_filename_list, test_csv_path)
+
+        # testに使用するsessionの抽出
+        if test_set_trial != None:
+            test_set = df_test[df_test['session_id'].isin(test_set_trial)]
+        else:
+            test_set = df_test
+
+        print("CSV->DataFrameへの変換完了")
+
+        # 特徴量とラベルの分離
+        pl = MlPipeline(self.consts)
+        X_test = pl.pick_features_pipeline.fit_transform(test_set)
+        y_test = pl.pick_label_pipeline.fit_transform(test_set)
+        print('特徴量とラベルの分離完了')
+
+        print(y_test)
+        return
+
+        # モデルの読み込み
+        if input_pkl_filename == None:
+            # fit()で生成したpklファイルを使用
+            input_pkl_filename = self.output_pkl_filename
+        input_pkl_filepath = self.consts.OUTPUT_PKL_PATH + '/' + input_pkl_filename
+        model = joblib.load(input_pkl_filepath)
+        print(f'fitting済みのモデルを読み込む: {input_pkl_filepath}')
+
+        # 学習
+        predicted_label = model.predict(X_test)
+
+        # 正答率の算出
+        # accuracy
+        accuracy = accuracy_score(y_test, predicted_label)
+        print(f'accuracy_score: {accuracy}')
+        # balanced_accuracy
+        balanced_accuracy = balanced_accuracy_score(y_test, predicted_label)
+        print(f'balanced_accuracy_score: {balanced_accuracy}')
+        # f1
+        f1 = f1_score(y_test, predicted_label)
+        print(f'f1_score: {f1}')
+        # confusion matrix
+        conf_mat = confusion_matrix(y_test, predicted_label)
+        print(f'confusion matrix: {conf_mat}')
+
+        # 記録
+        fname = input_pkl_filename
+        record = RecExpResultToMarkdown(fname, self.consts, accuracy, balanced_accuracy,
+                                        f1, conf_mat, input_pkl_filepath, self.train_filename_list, test_filename_list)
+        record.write()
 
     def main(self):
         # [training data] csv -> DataFrame
@@ -87,7 +154,7 @@ class MlPred():
         print('特徴量とラベルの分離完了')
 
         # 学習
-        model = joblib.load(self.input_pkl_path)
+        model = joblib.load(self.input_pkl_filename)
         model.fit(X_train, y_train)
         print('学習完了')
 
@@ -112,7 +179,7 @@ class MlPred():
         # 記録
         fname = ''
         record = RecExpResultToMarkdown(fname, self.consts, accuracy, balanced_accuracy,
-                                        f1, conf_mat, self.input_pkl_path, self.train_filename_list, self.test_filename_list)
+                                        f1, conf_mat, self.input_pkl_filename, self.train_filename_list, self.test_filename_list)
         record.write()
 
 
@@ -120,16 +187,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--train_files', nargs='+', required=True)
     parser.add_argument('--test_files', nargs='+', required=True)
-    parser.add_argument('--input_pkl_path', default=None)
-    parser.add_argument('--output_pkl_path', default=None)
+    parser.add_argument('--input_pkl_filename', default=None)
+    parser.add_argument('--output_pkl_filename', default=None)
     parser.add_argument('--csv_filename', default=None)
 
     args = parser.parse_args()
 
     eng = MlPred(train_filename_list=args.train_files, test_filename_list=args.test_files,
-                 input_pkl_path=args.input_pkl_path, output_pkl_path=args.output_pkl_path, csv_filename=args.csv_filename)
+                 input_pkl_filename=args.input_pkl_filename, output_pkl_filename=args.output_pkl_filename, csv_filename=args.csv_filename)
     # wav -> csv
-    # eng.wav2csv()
+    eng.wav2csv()
 
     # fitting
-    eng.fit()
+    # eng.fit()
